eb847d8a8697bbf415c277213d855da367d7bcc4
Kara O <karajo.odle@gmail.com>
2019-05-26 21:04:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def dictionary
+ {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => '@',
+    "and" => '&',
+  }
+end
+
+def word_substituter(tweet)
+  d = dictionary
+  tweet_array = tweet.split(/\b/)
+  new_arr = tweet_array.collect do |x|
+    lcX = x.downcase
+    d.key?(lcX) ? d[lcX] : x
+
+  end
+  new_arr.join('')
+end    
+
+def bulk_tweet_shortener(tweet_array)
+  tweet_array.each do |x|
+    puts word_substituter(x)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+  
+  
